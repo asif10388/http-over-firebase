0aa1c0ddafbc4de48385270eceabbf465d59e21c
asif10388 <asif.khan10388@gmail.com>
2023-03-14 23:16:30 +0600

    updated code to reflect dynamic behavior

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,41 @@
 import axios from "axios";
 import * as admin from "firebase-admin";
 
-const serviceAccount = require("./serviceAccountKey.json");
+import dotenv from "dotenv";
+dotenv.config();
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://taskventure001-default-rtdb.firebaseio.com/",
+  credential: admin.credential.cert({
+    projectId: process.env.PROJECT_ID,
+    clientEmail: process.env.CLIENT_EMAIL,
+    privateKey: (process.env.PRIVATE_KEY || "").replace(/\\n/g, "\n"),
+  }),
+  databaseURL: process.env.DATABASE_URL,
 });
 
 const db = admin.database();
 const documentsRef = db.ref("/req");
 
 documentsRef.limitToLast(1).on("child_added", async (snapshot) => {
-  const request = snapshot.val();
-  console.log("Request -->", request);
+  const req = snapshot.val().req;
+  const key = snapshot.key;
+  const { url, method, headers, body } = req;
 
   try {
     const response = await axios({
-      url: `http://localhost:32000/douglas`,
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      data: request,
+      url,
+      method,
+      headers,
+      data: body,
     });
+
     console.log(response.data);
+
+    await admin.database().ref(`res/${key}`).set({
+      headers: response.headers,
+      body: response.data,
+    });
+
     snapshot.ref.update({ response: response.data });
   } catch (error: any) {
     console.log("Something went wrong -->", error.message);
